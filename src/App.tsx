858eda30a58a64c312329a260ceb9e28f6496eed
izumi <izumitku@outlook.com>
2026-06-14 16:34:38 +0900

    feat: add routing for note detail page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Layout from "./Layout"
 import Home from "./pages/Home"
+import NoteDetail from "./pages/NoteDetail"
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />}></Route>
+            <Route path="/notes/:id" element={<NoteDetail/>}></Route>
           </Route>
         </Routes>
       </div>
