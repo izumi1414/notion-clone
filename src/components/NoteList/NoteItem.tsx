4906e94955b32f148b57c397b8b9da53ea33ddc9
izumi <izumitku99@gmail.com>
2026-08-24 09:54:21 +0900

    feat: implement child note creation

--- src/components/NoteList/NoteItem.tsx
+++ src/components/NoteList/NoteItem.tsx
@@ -14,12 +14,14 @@ import {
 } from 'react-icons/fi';
 import Item from '../SideBar/Item';
 import type { Note } from '../../modules/notes/note.entity';
+import type React from 'react';
 
 interface Props {
-  note: Note
+  note: Note,
+  onCreate: (e: React.MouseEvent) => void;
 }
 
-export default function NoteItem({note}: Props) {
+export default function NoteItem({note, onCreate}: Props) {
   const menu = (
     <div className='note-item-menu-container'>
       <DropdownMenu>
@@ -40,7 +42,7 @@ export default function NoteItem({note}: Props) {
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-      <div className='note-item-menu-button' role='button' onClick={() => {}}>
+      <div className='note-item-menu-button' role='button' onClick={onCreate}>
         <FiPlus className='note-item-menu-icon' size={16} />
       </div>
     </div>
--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -1,3 +1,4 @@
+import { noteRepository } from '../../modules/notes/note.repository';
 import { useNoteStore } from '../../modules/notes/notes.state';
 import NoteItem from './NoteItem';
 
@@ -5,10 +6,17 @@ export default function NoteList() {
   const noteStore = useNoteStore();
   const notes = noteStore.getAll();
 
+  const createChild = async (e: React.MouseEvent, parentId: number) => {
+    e.preventDefault();
+    const newNote = await noteRepository.create({ parentId });
+    console.log(newNote);
+    noteStore.set([newNote]);
+  }
+
   return (
     <>
       {notes.map((note) => (
-        <NoteItem key={note.id} note={note} />
+        <NoteItem key={note.id} note={note} onCreate={(e)=>createChild(e, note.id)}/>
       ))}
     </>
   );
