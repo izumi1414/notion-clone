7640091dc418a8e99f079af49ecfec020364b744
izumi <izumitku99@gmail.com>
2026-08-24 13:55:43 +0900

    feat: implement fetching and displaying child notes inline

--- src/components/NoteList/NoteItem.tsx
+++ src/components/NoteList/NoteItem.tsx
@@ -15,13 +15,23 @@ import {
 import Item from '../SideBar/Item';
 import type { Note } from '../../modules/notes/note.entity';
 import type React from 'react';
+import { useState } from 'react';
+import type { IconType } from 'react-icons';
 
 interface Props {
   note: Note,
   onCreate: (e: React.MouseEvent) => void;
+  onExpand: (e: React.MouseEvent) => void;
+  layer?: number;
 }
 
-export default function NoteItem({note, onCreate}: Props) {
+export default function NoteItem({note, onCreate, onExpand, layer = 0}: Props) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const getIcon = (): IconType => {
+    return isHovered ? FiChevronRight : FiFile;
+  }
+
   const menu = (
     <div className='note-item-menu-container'>
       <DropdownMenu>
@@ -49,8 +59,12 @@ export default function NoteItem({note, onCreate}: Props) {
   );
 
   return (
-    <div role='button' style={{ paddingLeft: '12px' }}>
-      <Item label={note.title ?? '無題'} icon={FiChevronRight} trailingItem={menu} />
+    <div 
+      role='button'
+      style={{ paddingLeft: `${layer * 12 + 12}px` }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}>
+      <Item label={note.title ?? '無題'} icon={getIcon()} trailingItem={menu} onIconClick={onExpand}/>
     </div>
   );
 }
--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -1,8 +1,15 @@
+import type React from 'react';
 import { noteRepository } from '../../modules/notes/note.repository';
 import { useNoteStore } from '../../modules/notes/notes.state';
 import NoteItem from './NoteItem';
+import type { Note } from '../../modules/notes/note.entity';
 
-export default function NoteList() {
+interface Props {
+  layer?: number;
+  parentId?: number;
+}
+
+export default function NoteList({layer = 0, parentId}: Props) {
   const noteStore = useNoteStore();
   const notes = noteStore.getAll();
 
@@ -13,10 +20,24 @@ export default function NoteList() {
     noteStore.set([newNote]);
   }
 
+  const fetchChildren = async (e: React.MouseEvent, note: Note) => {
+    e.preventDefault();
+    const children = await noteRepository.find({ parentId: note.id });
+    if (children === null) return;
+    console.log(children);
+    noteStore.set(children);
+  }
+
   return (
     <>
-      {notes.map((note) => (
-        <NoteItem key={note.id} note={note} onCreate={(e)=>createChild(e, note.id)}/>
+      {notes.filter((note) => note.id === parentId).map((note) => (
+        <div key={note.id}>
+        <NoteItem
+          note={note}
+          onCreate={(e) => createChild(e, note.id)}
+          onExpand={(e) => fetchChildren(e, note)} />
+        <NoteList layer={layer + 1} parentId={note.id}/>
+        </div>
       ))}
     </>
   );
